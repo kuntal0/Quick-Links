fc259f81e47debb8938e5de824284975fbcf3873
kuntal <kuntal0@users.noreply.github.com>
2023-05-17 08:49:17 +0530

    second commit

---
+++ src/webparts/kuntalQuickLinks/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IKuntalQuickLinksWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'KuntalQuickLinksWebPartStrings' {
+  const strings: IKuntalQuickLinksWebPartStrings;
+  export = strings;
+}
